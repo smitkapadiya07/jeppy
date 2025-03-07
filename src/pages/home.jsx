e1c8f728fed7bf2c5f87d23a295bd0657fb556e9
KunjKapadiya2007 <kunjkapadiya007@gmail.com>
2025-03-07 11:07:29 +0530

    small changes

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,7 +3,6 @@ import Herosection from "../component/Home-Page/herosection.jsx";
 import Youtube from "../component/Home-Page/youtube.jsx";
 import OurProduct from "../component/Home-Page/our-product.jsx";
 import NewProduct from "../component/Home-Page/new-product.jsx";
-import Talk from "../component/global/talk.jsx";
 
 function Home() {
     return (
@@ -12,7 +11,6 @@ function Home() {
             <Youtube/>
             <OurProduct/>
             <NewProduct/>
-            <Talk/>
         </>
     );
 }
--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ProductsCategories from "../component/product/productsCategories.jsx";
 import Header from "../component/global/header.jsx";
-import bg1 from "../assets/contact/Img1.webp";
+import bg1 from "../assets/image/product/bg2.jpeg";
 
 function Product() {
 
     const breadCrumbs = [
-        {label:'We believe that the highest quality is achieved through the development of new generation processes: discover them.'},
+        {label:'Home',route: '/'},
+        {label:'Product',route: '/product'},
     ]
-
     return (
         <>
             <Header  bgImg={bg1} heading={'Products'} breadcrumbs={breadCrumbs}  />
